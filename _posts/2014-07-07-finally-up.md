19d954883a2e6ab9f0c384d3e5828b050ffca57e
Ryan Plyler <g.r.plyler@gmail.com>
2014-07-07 20:07:36 +0800

    Fixed repo

---
+++ _posts/2014-07-07-finally-up.md
@@ -0,0 +1,14 @@
+---
+layout: post
+title: Finally up and running
+date: 2014-07-07 02:18
+author: g.r.plyler
+---
+
+It's 2:18 in the morning and I finally have
+the beginnings for my personal Jekyll site
+and blog.
+I'm too tired right now to write anything more
+but i'll have more content coming soon.
+I just didn't want to leave it a brand new site
+with nothing on it.
---
+++ publish.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Building..."
+jekyll build
+echo "Done."
+
+echo "Publishing Locally..."
+rsync -a \
+--exclude=".*" \
+--exclude="publish.sh" \
+./ ../grplyler.github.io/
+echo "Done."
+
+echo "Publishing to hqlocal..."
+rsync -a \
+-e "ssh -l root -p 22003" \
+--exclude=".*" \
+--exclude="publish.sh" \
+--chown www-data:root \
+../grplyler.github.io/ 192.168.1.103:/var/www/
+echo "Done."
+
+echo "Publishing to github.io..."
+cd ../grplyler.github.io
+git add --all
+git commit
+git push origin master
+echo "Done."
